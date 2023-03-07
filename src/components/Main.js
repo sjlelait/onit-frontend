fb0da3b5a6eeda08b19782ea7fb991360f348a58
Steven Medina <smedina2494@gmail.com>
2023-03-06 23:24:53 -0500

    WIP: Continued work on FireBase. User can now send Json Web Token to the backend

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,32 @@
+import { useEffect } from "react";
 
 function Main(props) {
+
+  // FB - Important! Example of usint JWT to confirm a user is authorized
+
+  const getList = async() => {
+  
+    const API_URL = 'http://localhost:3001/list'
+    let token;
+
+    if(props.user){
+      token = await props.user.getIdToken();
+      const response = await fetch(API_URL, {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer ' + token
+        }
+      });
+      const data = await response.json();
+      // setList(data);
+        }
+    }
+
+    useEffect(() => {
+      getList();
+    }, [props.user]);
+
+// This ends here
     return (
      <h1>Test</h1>
     )
