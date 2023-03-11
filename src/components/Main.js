27ea742f2015ffe72285e4af469181ab4cc0657a
Steven Medina <smedina2494@gmail.com>
2023-03-11 10:43:52 -0500

    WIP verifies API connection to backend

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,37 @@
-import { useEffect } from "react";
+import { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 
 function Main(props) {
+  // FB - Important! Example of using JWT to confirm a user is authorized
+  //FB - This is a hypothetical request that has the neceesary token authorization components that are demonstrated at the end of the part 2 video.
 
-    // FB - Important! Example of using JWT to confirm a user is authorized
-//FB - This is a hypothetical request that has the neceesary token authorization components that are demonstrated at the end of the part 2 video.
-  const getList = async() => {
-  
-    const API_URL = 'http://localhost:3001/list'
+  const [tasks, setTasks] = useState(null);
+  const API_URL = 'http://localhost:3001/home';
+
+  const getTask = async () => {
     let token;
 
-    if(props.user){
+    if (props.user) {
       token = await props.user.getIdToken();
       const response = await fetch(API_URL, {
         method: 'GET',
         headers: {
-          'Authorization': 'Bearer ' + token
-        }
+          Authorization: 'Bearer ' + token,
+        },
       });
       const data = await response.json();
-      // setList(data);
-        }
+      setTasks(data);
+      console.log(data);
     }
+  };
 
-    useEffect(() => {
-      getList();
-    }, [props.user]);
+  useEffect(() => {
+    getTask();
+  }, [props.user]);
 
-// This ends here
-    return (
-     <h1>Test</h1>
-    )
-  }
-  
-  export default Main;
-  
+  // This ends here
+  return <h1>Test</h1>;
+}
+
+export default Main;
