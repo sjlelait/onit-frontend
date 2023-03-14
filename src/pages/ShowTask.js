cf45418224d0a25eb09640a79313dd33ca1694b3
Steven Medina <smedina2494@gmail.com>
2023-03-13 23:35:14 -0400

    WIP: Allows content to still render when there are no subtasks

--- src/pages/ShowTask.js
+++ src/pages/ShowTask.js
@@ -10,6 +10,7 @@ const ShowTask = () => {
     const response = await fetch(url);
     const data = await response.json();
     setTask(data);
+    console.log(data)
   };
 
   useEffect(() => {
@@ -21,7 +22,13 @@ const ShowTask = () => {
       <div>
         <h1>{task.title}</h1>
         <p>{task.description}</p>
-        <p>{task.subtask[0].name}</p>
+        {task.subtask && task.subtask.length > 0 ? (
+          task.subtask.map((subtask) => (
+            <p key={subtask.id}>{subtask.name}</p>
+          ))
+        ) : (
+          <p>No subtasks found.</p>
+        )}
         {console.log(task)}
       </div>
     );
