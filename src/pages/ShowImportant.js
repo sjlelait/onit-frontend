1c424bad869acf727987a717739df31824602470
Sara Le Lait <sjlelait@gmail.com>
2023-03-14 19:24:52 -0600

    wip: working showImportant page

--- src/pages/ShowImportant.js
+++ src/pages/ShowImportant.js
@@ -1,54 +1,42 @@
-import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { Link } from 'react-router-dom';
 
-const ShowImportant = (props) => {
-    const { category } = useParams();
-    const url =  'http://localhost:3001/tasks/important';
-  
-  // State to hold the list data
-    const [list, setList] = useState(null);
+const ShowImportant = () => {
+    const url = "http://localhost:3001/tasks/important";
+    const [task, setTask] = useState([]);
 
- // fetch category data
-    const getList = async () => {
+ const getTask = async () => {
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data)
-      setList(data);
-    }
-  
-    useEffect(() => {
-      getList();
-    }, []);
-  
-    //Loaded function for when data is fetched
-    const loaded = () => {
-      return (
-          <div>
-        <h1>{category} List</h1>
-        <ul>
-          {list.map((item) => (
-              <Link to={`/tasks/${item._id}/subtasks`}>
-            <li key={item.id}>{item.title} {item.timeframe} {item.important ? 'important' : 'unimportant'} {item.complete ? "done" : "need to do" }</li>
-            </Link>
-          ))}
-        </ul>
-      </div>
-      );
+      setTask(data);
     };
-  
-  
-  
-    const loading = () => {
-      return <h1>Loading...</h1>;
-    };
-   
-    return (
-      <section className="task-section">
-          {list ? loaded() : loading()}
-       </section>
-    );
+
+  useEffect(() => {
+    getTask();
+  }, []);
+
+const loaded = () => {
+  return (
+    <div>
+      <h1>Important Tasks</h1>
+      <ul>
+        {task.map(task => (
+          <li key={task._id}>
+            <Link to={`/tasks/${task._id}/subtasks`}>{task.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+const loading = () => {
+    return <h1>Loading...</h1>;
+  };
+  return task ? loaded() : loading();
+}
+
+export default ShowImportant;
   
-    export default ShowImportant;
+  
+
   
