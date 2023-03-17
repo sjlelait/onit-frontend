7f77a086ae5c8c9d9be6e1df8ee18fc6bfb4764e
Steven Medina <smedina2494@gmail.com>
2023-03-16 22:47:52 -0400

    Feat: Fixes delete route

--- src/helper.js
+++ src/helper.js
@@ -1,29 +1,41 @@
 const url = 'http://localhost:3001';
 
-export const deleteItem = async (itemId) => {
-    try {
-        const response = await fetch(`${url}/tasks/${itemId}`, {
-          method: 'DELETE',
-        });
-        return true;
-    } catch (error) {
-        console.error('Error:', error);
-        return false;
+export const deleteItem = async (itemId, user) => {
+  console.log('Deleting item with id:', itemId);
+
+  try {
+    const token = await user.getIdToken();
+    const response = await fetch(`${url}/tasks/${itemId}`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': 'Bearer ' + token,
+      },
+    });
+
+    if (response.ok) {
+      return true;
+    } else {
+      console.error('Error:', response.statusText);
+      return false;
     }
-}
+  } catch (error) {
+    console.error('Error:', error);
+    return false;
+  }
+};
 
 export const editItem = async (itemId, newData) => {
-    try {
-        const response = await fetch(`${url}/tasks/${itemId}`, {
-          method: 'PUT',
-          body: JSON.stringify(newData),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        });
-        return true;
-    } catch (error) {
-        console.error('Error:', error);
-        return false;
-    }
-}
+  try {
+    const response = await fetch(`${url}/tasks/${itemId}`, {
+      method: 'PUT',
+      body: JSON.stringify(newData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return true;
+  } catch (error) {
+    console.error('Error:', error);
+    return false;
+  }
+};
