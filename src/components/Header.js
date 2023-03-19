dc4d06cbdc75cd4441b2d688d29a43a9f2839b95
Steven Medina <smedina2494@gmail.com>
2023-03-19 18:55:11 -0400

    Adds content to the landing page

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,35 +11,51 @@ function Header(props) {
 
   const handleLinkClick = () => {
     setExpanded(false);
-  }
+  };
 
   return (
-    <Navbar className="navbar" expanded={expanded} onToggle={setExpanded} expand="false">
-       <Navbar.Brand as={Link} to="/" className="navbar-brand-link">Onit</Navbar.Brand>
-      { props.user ?
+    <Navbar
+      className='navbar'
+      expanded={expanded}
+      onToggle={setExpanded}
+      expand='false'
+    >
+      <Navbar.Brand as={Link} to='/' className='navbar-brand-link'>
+        Onit
+      </Navbar.Brand>
+      {props.user ? (
         <>
-          <div className="user-info">
-            <p className="mb-0">Welcome, {props.user.displayName}</p>
+          <div className='user-info'>
+            <p className='mb-0'>Welcome, {props.user.displayName}</p>
           </div>
-          <div className="user-image-container">
+          <div className='user-image-container'>
             <img
               src={props.user.photoURL}
               alt={props.user.displayName}
-              className="ml-3 user-image"
+              className='ml-3 user-image'
             />
           </div>
-          <Navbar.Toggle className="hamburger" aria-controls="basic-navbar-nav"/>
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto hamburger-menu" onClick={handleLinkClick}>
-              <Nav.Link as={Link} to="/tasks/important">Important Tasks</Nav.Link>
-              <Nav.Link as={Link} to="/tasks/new">New Task</Nav.Link>
-              <Nav.Link onClick={logout}>Logout</Nav.Link>
+          <Navbar.Toggle
+            className='hamburger'
+            aria-controls='basic-navbar-nav'
+          />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='mr-auto hamburger-menu' onClick={handleLinkClick}>
+              <Nav.Link as={Link} to='/tasks/important'>
+                Important Tasks
+              </Nav.Link>
+              <Nav.Link as={Link} to='/tasks/new'>
+                New Task
+              </Nav.Link>
+              <Nav.Link onClick={logout}>
+                Logout
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </>
-        :
-        <Nav.Link onClick={login}>Login</Nav.Link>
-      }
+      ) : (
+        <Nav.Link className='navbar-brand-link' onClick={login}>Login</Nav.Link>
+      )}
     </Navbar>
   );
 }
